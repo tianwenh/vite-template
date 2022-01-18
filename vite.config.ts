2628da55269a5613b470fce48d507035b99b423e
tianwenh <huastronmy@gmail.com>
2022-01-17 19:23:36 -0800

    use vite-plugin-ssgpage instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mdx from '@mdx-js/rollup';
-import remarkGfm from 'remark-gfm';
-import remarkFrontmatter from 'remark-frontmatter';
-import rehypeSlug from 'rehype-slug';
-import rehypePrism from 'mdx-prism';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import { mdx } from '@tianwenh/vite-plugin-ssgpage';
 
 export default defineConfig({
-  plugins: [
-    mdx({
-      remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMath],
-      rehypePlugins: [
-        rehypeKatex,
-        rehypePrism,
-        rehypeSlug,
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: { class: 'header-anchor' },
-            content: {
-              type: 'text',
-              value: '#',
-            },
-          },
-        ],
-      ],
-      jsxRuntime: 'classic',
-    }),
-    react(),
-  ],
+  plugins: [mdx()],
 });
